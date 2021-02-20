5388577c7e82b2a6952357d2a57810a1b956338b
Arely Aceves <arelyac01@gmail.com>
2021-02-19 22:43:10 -0600

    Updated Estructuras de Datos md and main.py.

--- Tarea1/main.py
+++ Tarea1/main.py
@@ -1,11 +1,16 @@
+"""
+Stack
+"""
 from Stack import Stack
+
+print("\nStack\n")
 
 stack = Stack(["Eric", "John", "Michael"])
 empty_stack = Stack()
 stack = Stack(["Eric", "John", "Michael"])
 empty_stack = Stack()
 print(stack)
-#>>['Eric', 'John','Michael']
+#>> ['Eric', 'John','Michael']
 print(empty_stack)
 #>> []
 
@@ -19,31 +24,40 @@ print(stack.size())
 
 stack.push("Sophia")
 print(stack)
-#>>['Eric', 'John','Michael','Sophia']
+#>> ['Eric', 'John','Michael','Sophia']
 stack.push(["Anna","Arthur","Tom"])
 print(stack)
-#>>['Eric', 'John','Michael','Sophia','Anna','Arthur','Tom']
+#>> ['Eric', 'John','Michael','Sophia','Anna','Arthur','Tom']
 
 name = stack.pop()
 print(name)
 #>>Tom
 print(stack)
-#>>['Eric', 'John','Michael','Sophia','Anna','Arthur']
+#>> ['Eric', 'John','Michael','Sophia','Anna','Arthur']
+
+stack_copy = stack.copy()
+print(stack_copy)
+#>> ['Eric', 'John','Michael','Sophia','Anna','Arthur']
 
 stack.clear()
 print(stack)
 #>>[]
+
+"""
+Queue
+"""
+
 from Queue import Queue
 
+print("\nQueue\n")
+
 queue = Queue(["Eric", "John", "Michael"])
-empty_queue = Stack()
+empty_queue = Queue()
 print(queue)
-#>>['Eric', 'John','Michael']
+#>> ['Eric', 'John','Michael']
 print(empty_queue)
 #>> []
 
-queue = Queue(["Eric", "John", "Michael"])
-empty_queue = Stack()
 print(queue.empty())
 #>> True
 print(empty_queue.empty())
@@ -54,20 +68,107 @@ print(queue.size())
 
 queue.push("Sophia")
 print(queue)
-#>>['Eric', 'John','Michael','Sophia']
+#>> ['Eric', 'John','Michael','Sophia']
 queue.push(["Anna","Arthur","Tom"])
 print(queue)
-#>>['Eric', 'John','Michael','Sophia','Anna','Arthur','Tom']
+#>> ['Eric', 'John','Michael','Sophia','Anna','Arthur','Tom']
 
 name = queue.pop()
 print(name)
 #>>Eric
 print(queue)
-#>>['John','Michael','Sophia','Anna','Arthur','Tom']
+#>> ['John','Michael','Sophia','Anna','Arthur','Tom']
+
+queue_copy = queue.copy()
+print(queue_copy)
+#>> Queue(['John','Michael','Sophia','Anna','Arthur','Tom'])
 
 queue.clear()
 print(queue)
 #>>[]
 
-a = dict(one=1, two=2, three=3)
-print(a)
+"""
+Dictionary
+"""
+
+print("\nDictionary\n")
+
+dictionary = {'one': 1, 'two': 2, 'three': 3}
+print(dictionary)
+#>> {'one': 1, 'two': 2, 'three': 3}
+
+dictionary_compr = dict(one=1, two=2, three=3)
+print(dictionary_compr)
+#>> {'one': 1, 'two': 2, 'three': 3}
+
+dictionary_constr = dict(one=1, two=2, three=3)
+print(dictionary_constr)
+#>> {'one': 1, 'two': 2, 'three': 3}
+
+element = dictionary.setdefault('four', 4)
+print(element)
+#>> 4
+element = dictionary.setdefault('five')
+print(element)
+#>> None
+
+print('six' in dictionary)
+#>> False
+print('two' in dictionary)
+#>> True
+
+print('six' not in dictionary)
+#>> True
+print(not 'six' in dictionary)
+#>> True
+print('two' not in dictionary)
+#>> False
+print(not 'two' in dictionary)
+#>> False
+
+print(dictionary.keys())
+#>> dict_keys(['one', 'two', 'three', 'four', 'five'])
+
+print(list(dictionary))
+#>> ['one', 'two', 'three', 'four', 'five']
+
+print(dictionary.values())
+#>> dict_values([1, 2, 3, 4, None])
+
+element = dictionary.get('four', 6)
+print(element)
+#>> 4
+element = dictionary.get('six', 6)
+print(element)
+#>> 6
+element = dictionary.get('six')
+print(element)
+#>> None
+
+element = dictionary.pop('four', 6)
+print(element)
+#>> 4
+print(dictionary)
+#>> {'one': 1, 'two': 2, 'three': 3, 'five': None}
+element = dictionary.pop('six', 6)
+print(element)
+#>> 6
+print(dictionary)
+#>> {'one': 1, 'two': 2, 'three': 3, 'five': None}
+try:
+    element = dictionary.pop('six')
+except KeyError:
+    print(KeyError)
+
+del dictionary['five']
+print(dictionary)
+#>> {'one': 1, 'two': 2, 'three': 3}
+try:
+    del dictionary['five']
+except KeyError:
+    print(KeyError)
+#>> <class 'KeyError'>
+
+dictionary.update({'four': 1, 'five': 2, 'six': 3})
+print(dictionary)
+#>> {'one': 1, 'two': 2, 'three': 3, 'four': 1, 'five': 2, 'six': 3}
